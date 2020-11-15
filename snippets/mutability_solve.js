a138ce146e645ac4e670e1ee7183f02766413b56
franckf <efydd.artio@tuta.io>
2020-11-15 18:00:22 +0100

    [FEAT] demo mutability

---
+++ snippets/mutability_solve.js
@@ -0,0 +1,9 @@
+const items = [2,1,3,6,4,5]
+
+console.log("original :", items)
+
+const sorted = [...items].sort()
+
+console.log("sorted array :", sorted)
+console.log("original is still the same :", items)
+
---
+++ snippets/mutability_warning.js
@@ -0,0 +1,9 @@
+const items = [2,1,3,6,4,5]
+
+console.log("original :", items)
+
+const sorted = items.sort()
+
+console.log("sorted array :", sorted)
+console.log("original has been changed :", items)
+
