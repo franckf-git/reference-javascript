3ee58e34ae4fd4f10333140dcdb42b6efa96b4d6
franckf <>
2021-11-10 07:16:30 +0100

    chores: move to own repo

--- full-project-examples/lite-api-crud/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ENV: process.env.NODE_ENV || 'production',
-    PORT: process.env.PORT || 3000,
-    TOKEN_SECRET: 'qbvrjkj42545kqzervhj442wrgkfhws5f4ze',
-    TOKEN_EXPIRATION: '1d',
-    SQLITE_FICHIER: './production.sqlite'
-}
